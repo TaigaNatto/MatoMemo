1926a68fb55af5bfcec732ace261ca95dda2833e
kana0rai <tenmanokocto@yahoo.co.jp>
2017-06-12 18:17:21 +0900

    5番のレイアウトを書きました。

--- app/src/main/java/com/example/t_robop/matomemo/MainActivity5.java
+++ app/src/main/java/com/example/t_robop/matomemo/MainActivity5.java
@@ -1,17 +1,13 @@
 package com.example.t_robop.matomemo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FirstActivity extends AppCompatActivity {
+public class MainActivity5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
-
-        Intent intent =new Intent(this,MainActivity5.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_main5);
     }
 }
