0b6cfc7f1802c9c6eee2dfb66ae855c54fcc8ee6
Dai1678 <dai7774@gmail.com>
2017-06-12 19:59:35 +0900

    manifestsの修正

--- app/src/main/java/com/example/t_robop/matomemo/FirstActivity.java
+++ app/src/main/java/com/example/t_robop/matomemo/FirstActivity.java
@@ -1,7 +1,21 @@
 package com.example.t_robop.matomemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +23,9 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+
     }
+
+
 }
