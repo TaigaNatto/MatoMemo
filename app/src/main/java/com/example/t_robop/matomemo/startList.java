d0aa7145503398f50df21f91a453709f8949fc36
Kawaguchi410 <kawakawa.410487@gmail.com>
2017-06-14 18:59:57 +0900

    startListActivityとgroupCreateActivityの実装

---
+++ app/src/main/java/com/example/t_robop/matomemo/startList.java
@@ -0,0 +1,14 @@
+package com.example.t_robop.matomemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class startList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_list);
+
+    }
+}
