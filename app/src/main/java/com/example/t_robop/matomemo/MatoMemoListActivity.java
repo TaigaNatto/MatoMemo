a3e95aa87a59ad871252aff28cac8d02e200d079
Dai1678 <dai7774@gmail.com>
2017-06-19 21:35:33 +0900

    MatoMemoListActivityの下部にButtonを追加 タブごとにテキスト表示を変えるのがきついので、フローティングアクションボタンに変えたい

--- app/src/main/java/com/example/t_robop/matomemo/MatoMemoListActivity.java
+++ app/src/main/java/com/example/t_robop/matomemo/MatoMemoListActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -120,6 +121,7 @@ public class MatoMemoListActivity extends AppCompatActivity implements ViewPager
 
             switch (page){
                 case 1:
+
                     listView = (ListView)inflater.inflate(R.layout.activity_memo_tab,container,false);
 
                     ArrayList<String> memoList = new ArrayList<>();
